f7408501abca0e6c71d4393fcc62c93e7477f9b1
dudgns3443 <williams3443@gmail.com>
2021-12-22 17:30:12 +0900

    .

--- scripts/custom.js
+++ scripts/custom.js
@@ -5,7 +5,7 @@ setTimeout(function(){
 },150);
 document.addEventListener('DOMContentLoaded', () => {
     'use strict'
-    var url = '/nlb/'
+    var url = 'nlb/'
     //Global Variables
     let isPWA = true;  // Enables or disables the service worker and PWA
     let isAJAX = true; // AJAX transitions. Requires local server or server
@@ -141,7 +141,7 @@ document.addEventListener('DOMContentLoaded', () => {
         //menu('menu-settings', 'show', 250);
 
         //signup
-        console.log(window.location.hostname+'/nlb/')
+ 
         var signupBtn = document.querySelector('#signupBtn')
         if(signupBtn){
             // console.log(window.location.href)
